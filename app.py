5edb0f978ac965f4dbc7a6ceebe0a15005b44181
ZakM3 <zakaria.mahamed1@gmail.com>
2022-09-26 13:41:50 -0500

    done

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+sns.set()
+import matplotlib.pyplot as plt
+import plotly.express as px
+import numpy as np
+
+st.title("supermarket data")
+df = pd.read_csv('supermarket.csv')
+st.subheader('Top 10 performing stores')
+df.sort_values(["store_sales"], 
+axis=0, 
+ascending=[False],
+inplace=True)
+st.dataframe(df[["store_id","store_sales"]].head(n=10))
+
+
+st.subheader('Lowest 10 performing stores')
+df.sort_values(["store_sales"], 
+axis=0, 
+ascending=[True],
+inplace=True)
+st.dataframe(df[["store_id","store_sales"]].head(n=10))
+
+
+st.subheader("Average Sale Per Customer") #gauge
+st.metric(label="USD", value="21.00")
+st.header("Average Sale Per Customer Overall") #gauge
+st.metric(label="USD", value="75.00")
+
